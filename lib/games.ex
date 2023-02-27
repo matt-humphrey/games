f5df9798021e449eeb73f4e4c43237dcb1d6185c
Matt Humphrey <mhumphrey9@hotmail.com>
2023-02-27 10:35:49 +0800

    finish guessing game exercise

--- lib/games.ex
+++ lib/games.ex
@@ -2,17 +2,34 @@ defmodule Games do
   @moduledoc """
   Documentation for `Games`.
   """
+end
 
+defmodule Games.GuessingGame do
   @doc """
-  Hello world.
+  A guessing game, where the user guesses a number between 1 and 10.
 
   ## Examples
 
-      iex> Games.hello()
-      :world
+      iex> Games.GuessingGame.play()
 
   """
-  def hello do
-    :world
+  def play() do
+    play(Enum.random(1..10))
+  end
+
+  def play(number) do
+    guess = IO.gets("Enter your guess: ")
+    |> String.trim()
+    |> String.to_integer()
+    |> _check(number)
+  end
+
+  defp _check(number, number), do: "Correct!"
+  defp _check(guess, number) do
+    cond do
+      guess > number -> IO.puts "Too High!"
+      guess < number -> IO.puts "Too Low!"
+    end
+    play(number)
   end
 end
